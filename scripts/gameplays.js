e289ee549dc0f9935ba887eba0df048c09a9eea4
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-07-10 13:12:18 -0300

    Adição do label para usuário do gameplay

--- scripts/gameplays.js
+++ scripts/gameplays.js
@@ -1,4 +1,4 @@
-let listaDeArquivos = ["video1", "video2", "video3", "video4", "video5"];
+let listaDeArquivos = [["user1","video1"], ["user2","video2"], ["user3","video3"], ["user4","video4"], ["user5","video5"]];
 let secaoQuery = document.querySelector('.video');
 let meusVideosQuery = document.querySelector('.meus-videos');
 let uploadQuery = document.querySelector('#upload-btn');
@@ -25,8 +25,9 @@ function salvaVideo() {
 }
 
 for (video of listaDeArquivos) {
-	let videosrc = diretorio + video + ".mp4";
-	secaoQuery.innerHTML += `<div class="componente-video"><video width="320" height="240" controls><source src=${videosrc} type="video/mp4"></video></div>`;
+	let user = video[0];
+	let videosrc = diretorio + video[1] + ".mp4";
+	secaoQuery.innerHTML += `<div class="componente-video"><video width="320" height="240" controls><source src=${videosrc} type="video/mp4"></video><p><label for="video" class="user-label">${user}</p></div>`;
 }
 
 uploadQuery.addEventListener('click', salvaVideo);
